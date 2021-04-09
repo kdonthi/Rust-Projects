e3ba958063d2390d44ce950f1662a40571b51274
Kaushik Donthi <kaushikdr@ucla.edu>
2021-04-09 14:47:26 -0700

    got html to download upon entering ip address

--- html_server/src/main.rs
+++ html_server/src/main.rs
@@ -1,10 +1,12 @@
-use std::num::ParseIntError;
+//use std::num::ParseIntError;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::io::Write;
 use std::io::Read;
 use std::vec::Vec;
-fn reply(request: &mut TcpStream) -> String {
+use std::fs::File;
+use std::io::Error;
+fn get_request(request: &mut TcpStream) -> String {
     //let outgoing = request.accept();
     /*let mut outgoing = match outgoing {
         Ok((TcpStream, socketaddr)) => TcpStream,
@@ -12,13 +14,14 @@ fn reply(request: &mut TcpStream) -> String {
     };*/
     let mut array: [u8; 1024] = [0; 1024];
     //let mut smartpointer = Box(array);
-    request.read(&mut array);
+    request.read(&mut array); //why does read need a mutable reference
     let stringReturn :String = String::from_utf8_lossy(&array).to_string(); //lossy converts from array while wo lossy converts from vector
     //what is the point of doing [..] if we are not having a string?
     println!("{}", stringReturn);
-    return (stringReturn);
-}   
-fn main () {
+    return stringReturn;
+}
+
+fn main () -> Result<(), Error> {
     //FtpListner -> bind onto the IP address we want to listen on //so where is the server being created?
     //is an ip address just a port where a connection CAN arrive at (like a port is a place a ship can dock?)
     //is a server just something that listens to incoming connections on an IP address and responds?
@@ -29,26 +32,50 @@ fn main () {
         Ok(TCPListener) => TCPListener,
         Err(err) => panic!("{:?}", err),
     };
-    let mut requestCopy;
-    let mut counter: i8 = 0;
-    for request in listener.incoming() {
-        requestCopy = request;
-        println!("{:?}", requestCopy);
-        let mut requestCopy = match requestCopy {
+    let mut streamCopy;
+    let mut counter: i8;
+
+    for stream in listener.incoming() { //any way to make request mut?
+        streamCopy = stream;
+        println!("{:?}", streamCopy);
+        let mut streamCopy = match streamCopy {
             Ok(stream) => stream,
             Err(err) => panic!("Error in listening!"),
         };
-        let requestCopy = reply(&mut requestCopy);
-        let responseVector: Vec<char> = Vec::new();
-        for i in requestCopy.split("\n") {
-            println!("{}", i);
-            counter += 1;
-            println!("\nCounter: {}", counter);
-            if (counter >= 3) {
-                break;
-            }
-        }
         counter = 0;
+        let mut requestString = get_request(&mut streamCopy);
+        let mut responseVector: Vec<char> = Vec::new();
+        for i in requestString.split("\n") {
+            if counter == 0 {
+                for (counter, j) in (i.split(" ")).enumerate() {
+                    //println!("Counter: {}, Val: {}", counter ,j);
+                    if counter == 2 {
+                        println!("{:?}", j.trim().chars());
+                        responseVector.extend(j.trim().chars()); //putting first line in HTML response
+                        break;
+                    }
+                }
+                //panic!("G5 and we live!");
+                //responseVector.extend(i.split(" ")[2].chars()); //putting first line in HTML response
+                responseVector.extend(" 200 OK\r\n".chars());
+            }
+            else {
+                responseVector.extend(i.chars()); //putting headers
+                responseVector.extend("\n".chars());
+            }
+            counter += 1;
+            //println!("\nCounter: {}", counter);
+        }
+        let htmlfile = File::open("sampleHTML.html");
+        let mut contents = String::new();
+        htmlfile?.read_to_string(&mut contents);
+        responseVector.extend(contents.chars());
+        println!("{:?}", responseVector.iter().collect::<String>());
+        match streamCopy.write(&responseVector.iter().collect::<String>().as_bytes()[..]) {
+            Ok(n) => n,
+            Err(err) => panic!("{}", err),
+        };
+        //streamCopy.write(&responseVector[..]);
     }
-
+    Ok(())
 }
