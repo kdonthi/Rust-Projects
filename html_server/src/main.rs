d7a17ff1f868e4eb8571e3b0482d44145125a947
Kaushik Donthi <kaushikdr@ucla.edu>
2021-04-10 14:25:47 -0700

    Got 100 threads

--- html_server/src/main.rs
+++ html_server/src/main.rs
@@ -6,17 +6,12 @@ use std::io::Read;
 use std::vec::Vec;
 use std::fs::File;
 use std::io::Error;
+use std::thread;
+use threadpool::ThreadPool;
 fn get_request(request: &mut TcpStream) -> String {
-    //let outgoing = request.accept();
-    /*let mut outgoing = match outgoing {
-        Ok((TcpStream, socketaddr)) => TcpStream,
-        Err(err) => panic!("{}", err),
-    };*/
     let mut array: [u8; 1024] = [0; 1024];
-    //let mut smartpointer = Box(array);
     request.read(&mut array); //why does read need a mutable reference
     let stringReturn :String = String::from_utf8_lossy(&array).to_string(); //lossy converts from array while wo lossy converts from vector
-    //what is the point of doing [..] if we are not having a string?
     println!("{}", stringReturn);
     return stringReturn;
 }
@@ -37,61 +32,61 @@ fn strcmp(str1: &str, str2: &str) -> bool {
     return true;
 }
 
+fn handleStream(stream: Result<TcpStream, Error>) -> Result<(), Error> {
+    let mut streamCopy = stream;
+    let mut streamCopy = match streamCopy {
+        Ok(n) => n,
+        Err(err) => panic!("{}", err),
+    };
+    let mut requestString = get_request(&mut streamCopy);
+    let mut responseVector: Vec<char> = Vec::new();
+    let mut responseString: String;
+    let mut htmlfile;
+    let mut contents = String::new();
+    for i in requestString.split("\n") {
+        println!("{}", i);
+        
+        for (counter, j) in (i.split(" ")).enumerate() {
+            if counter == 2 {
+                if (strcmp(i, "GET / HTTP/1.1\r")) {
+                    htmlfile = File::open("correctHTML.html");
+                    htmlfile?.read_to_string(&mut contents);
+                    responseVector.extend(j.trim().chars());
+                    responseVector.extend(" 200 OK\r\nContent-Type: text/html\n".chars());
+                }
+                else { 
+                    htmlfile = File::open("errorHTML.html");
+                    htmlfile?.read_to_string(&mut contents);
+                    responseVector.extend(j.trim().chars());
+                    responseVector.extend(" 404 NOT FOUND\r\nContent-Type: text/html\n".chars());
+                }
+                break;
+            }
+        }
+        break;
+    }
+    responseVector.extend(contents.chars());
+    match streamCopy.write(&responseVector.iter().collect::<String>().as_bytes()[..]) {
+        Ok(n) => n,
+        Err(err) => panic!("{}", err),
+    };
+    Ok(())
+}
 fn main () -> Result<(), Error> {
     let listener = TcpListener::bind("127.0.0.1:7878");
     let listener = match listener {
         Ok(TCPListener) => TCPListener,
         Err(err) => panic!("{:?}", err),
     };
-    let mut streamCopy;
     let mut counter: i8;
+    let pool = ThreadPool::new(100);
 
     for stream in listener.incoming() { //any way to make request mut?
-        streamCopy = stream;
-        println!("{:?}", streamCopy);
-        let mut streamCopy = match streamCopy {
-            Ok(strm) => strm,
-            Err(err) => panic!("Error in listening!"),
-        };
-        counter = 0;
-        let mut requestString = get_request(&mut streamCopy);
-        let mut responseVector: Vec<char> = Vec::new();
-        let mut responseString: String;
-        let mut htmlfile;
-        let mut contents = String::new();
-        for i in requestString.split("\n") {
-            println!("{}", i);
-            
-            for (counter, j) in (i.split(" ")).enumerate() {
-                if counter == 2 {
-                    if (strcmp(i, "GET / HTTP/1.1\r")) {
-                        htmlfile = File::open("correctHTML.html");
-                        htmlfile?.read_to_string(&mut contents);
-                        responseVector.extend(j.trim().chars());
-                        responseVector.extend(" 200 OK\r\nContent-Type: text/html\n".chars());
-                    }
-                    else { 
-                        htmlfile = File::open("errorHTML.html");
-                        htmlfile?.read_to_string(&mut contents);
-                        responseVector.extend(j.trim().chars());
-                        responseVector.extend(" 404 NOT FOUND\r\nContent-Type: text/html\n".chars());
-                    }
-                    //responseVector.extend(j.trim().chars()); //putting first line in HTML response
-                    break;
-                }
-            }
-            break;
-        }
-        responseVector.extend(contents.chars());
-        match streamCopy.write(&responseVector.iter().collect::<String>().as_bytes()[..]) {
-            Ok(n) => n,
-            Err(err) => panic!("{}", err),
-        };
-        /*match streamCopy.write(&responseVector.iter().collect::<String>().as_bytes()[..]) {
-            Ok(n) => n,
-            Err(err) => panic!("{}", err),
-        };*/
-        //streamCopy.write(&responseVector[..]);
+        println!("Made it to handle stream!");
+        pool.execute(|| {
+            handleStream(stream);
+            });
     }
+    pool.join();
     Ok(())
 }
